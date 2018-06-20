94ab61b8ed1d92f2fa1f60780ad41502f134f6a6
Afraoo <moloop@outlook.com>
2018-06-20 11:11:34 +0800

    AddTwoNumbers
    Waiting to be completed

---
+++ AddTwoNumbers.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+class ListNode
+{
+    int val;
+    ListNode next;
+
+    public ListNode(int x){
+        val=x;
+    }
+
+}
+
+public class AddTwoNumbers {
+    public static  void main(String[] args) {
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode pre = l1;
+        ListNode after = l2;
+        ListNode curr = new ListNode(0);
+        if(l1.next != null){
+
+        }
+
+        return null;
+    }
+}
+
+
